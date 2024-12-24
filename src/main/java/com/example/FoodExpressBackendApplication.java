6d87ec759ba2ed66265293de3e43e59b3914510b
RahulSonpaliya <rahulsonpaliya@gmail.com>
2024-12-24 16:25:52 +0530

    Initial commit to sync all files

---
+++ src/main/java/com/example/FoodExpressBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodExpressBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodExpressBackendApplication.class, args);
+	}
+
+}
