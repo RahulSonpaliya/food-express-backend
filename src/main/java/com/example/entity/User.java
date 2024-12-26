d66c5bb8af537d5fcad1dae8cc3a767fb116a6ec
RahulSonpaliya <rahulsonpaliya@gmail.com>
2024-12-26 18:14:08 +0530

    Implements User entity with MongoDB persistence.

--- src/main/java/com/example/entity/User.java
+++ src/main/java/com/example/entity/User.java
@@ -1,13 +1,22 @@
 package com.example.entity;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import com.example.dto.AccountType;
 
 import lombok.Data;
 
 @Data
+@Document(collection = "users")
+@CompoundIndex(name = "email_accountType_idx", def = "{'email': 1, 'accountType': 1}", unique = true)
 public class User {
+	@Id
 	private String id;
 	private String name;
+	@Indexed(unique = true)
 	private String email;
 	private String password;
 	private AccountType accountType;
