87ab015fca87be496c35fb9ca50f825fe5e4b997
RahulSonpaliya <rahulsonpaliya@gmail.com>
2025-09-26 10:54:25 +0530

    Added validations in AddToCartRequest

--- src/main/java/com/example/model/request/AddToCartRequest.java
+++ src/main/java/com/example/model/request/AddToCartRequest.java
@@ -1,5 +1,7 @@
 package com.example.model.request;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,7 +10,17 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class AddToCartRequest {
+
+    @NotNull(message = "{quantity.invalid}")
+    @Min(value = 1, message = "{quantity.invalid}")
     private Long quantity;
+
+    @NotNull(message = "{productId.invalid}")
+    @Min(value = 1, message = "{productId.invalid}")
     private Long productId;
+
+    @NotNull(message = "{variantId.invalid}")
+    @Min(value = 1, message = "{variantId.invalid}")
     private Long variantId;
 }
+
