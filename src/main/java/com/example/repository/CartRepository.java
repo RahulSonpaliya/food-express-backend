4cf3c5f3466a9bd3ea31e8508b2c20589f2598f2
RahulSonpaliya <rahulsonpaliya@gmail.com>
2025-09-29 09:55:29 +0530

    Added Cart repository

---
+++ src/main/java/com/example/repository/CartRepository.java
@@ -0,0 +1,10 @@
+package com.example.repository;
+
+import com.example.entity.Cart;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartRepository extends MongoRepository<Cart, Long> {
+
+}
