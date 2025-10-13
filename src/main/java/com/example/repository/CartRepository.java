53f156f7dfe1d30c06d54a29f8726705fad1047a
RahulSonpaliya <rahulsonpaliya@gmail.com>
2025-10-13 10:25:39 +0530

    Added findByUserId method in CartRepository

--- src/main/java/com/example/repository/CartRepository.java
+++ src/main/java/com/example/repository/CartRepository.java
@@ -4,7 +4,9 @@ import com.example.entity.Cart;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CartRepository extends MongoRepository<Cart, Long> {
-
+    Optional<Cart> findByUserId(Long userId);
 }
